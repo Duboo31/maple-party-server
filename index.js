ea6670a713fec8309195b322629937edbc0bc073
hyun <duboo31@gmail.com>
2024-11-26 06:33:34 +0900

    Add config

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express();
 const port = 5000;
 import mongoose from "mongoose";
 
+import config from "./config/mongo.js";
+
 mongoose
-  .connect(`mongodb+srv://duboo31:@mapleparty.2ly3r.mongodb.net/`)
+  .connect(
+    `mongodb+srv://duboo31:${config.MONGO_PASSWORD}@mapleparty.2ly3r.mongodb.net/`
+  )
   .then(() => console.log("MongoDB 연결 성공..."))
   .catch((err) => console.log(err));
 
